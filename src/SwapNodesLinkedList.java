f5c325114fba603677240bf3963cf3b5b23ecc52
dharshan_bj <bjdharshan@gmail.com>
2018-04-20 11:29:04 -0700

    Swap node pairs using 2 temp nodes and dummy node

---
+++ src/SwapNodesLinkedList.java
@@ -0,0 +1,24 @@
+
+ //Definition for singly-linked list.
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+public class SwapNodesLinkedList {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode current=dummy;
+        while(current.next != null && current.next.next!=null){
+            ListNode first=current.next;
+            ListNode second=current.next.next;
+            first.next=second.next;
+            current.next=second;
+            second.next=first;
+            current=current.next.next;
+        }
+        return dummy.next;
+    }
+}
