4e03431a2347bb661b35caf800f84d457dd3e6f2
dharshan_bj <bjdharshan@gmail.com>
2018-04-21 13:36:44 -0700

    Application of stack in Linked List addition

---
+++ src/AddTwoNumbersLinkedList.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class AddTwoNumbersLinkedList {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> lst1=new Stack<Integer>();
+        Stack<Integer>lst2=new Stack<Integer>();
+        while(l1!=null){
+            lst1.push(l1.val);
+            l1=l1.next;
+        }
+        while(l2!=null){
+            lst2.push(l2.val);
+            l2=l2.next;
+        }
+        int sum=0;
+        ListNode node=new ListNode(0);
+        while(!lst1.isEmpty() || !lst2.isEmpty()){
+            if(!lst1.isEmpty()) sum+=lst1.pop();
+            if(!lst2.isEmpty()) sum+=lst2.pop();
+
+            node.val=sum%10;
+            ListNode head=new ListNode(sum/10);
+            head.next=node;
+            node=head;
+            sum/=10;
+        }
+        return node.val==0? node.next:node;
+    }
+}
